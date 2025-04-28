9cea82eb61b675b468b393a836b49987c0aa9b91
Simlex <simlexafol@gmail.com>
2025-04-28 15:52:14 +0100

    fix: enhance layout and styling for homepage sections, adjusting heights and padding for improved responsiveness

--- app/components/homepage/who-its-for.tsx
+++ app/components/homepage/who-its-for.tsx
@@ -71,12 +71,12 @@ const WhoItsForSection = () => {
             variants={containerVariants}
             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6"
           >
-            <div className="col-span-full h-[400px] relative">
-              <div className="absolute top-8 left-8 z-10">
-                <h4 className="text-3xl font-semibold mb-1">
+            <div className="col-span-full h-[250px] md:h-[400px] relative">
+              <div className="absolute p-5 md:p-0 md:top-8 md:left-8 z-10">
+                <h4 className="text-2xl md:text-3xl font-semibold mb-1">
                   Creators & Influencers
                 </h4>
-                <p className="w-[60%]">
+                <p className="w-full h:w-[60%]">
                   Transform your raw footage into studio-grade content in
                   seconds. Go viral, with intention.
                 </p>
@@ -88,12 +88,12 @@ const WhoItsForSection = () => {
                 className="object-cover"
               />
             </div>
-            <div className="col-span-full md:col-span-3 h-[400px] text-black relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[250px] before:bg-gradient-to-b before:from-[#F0C09A] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
-              <div className="absolute top-8 left-8 z-10">
-                <h4 className="text-3xl font-semibold mb-1">
+            <div className="col-span-full md:col-span-3 h-[250px] md:h-[400px] text-black relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[160px] before:md:h-[250px] before:bg-gradient-to-b before:from-[#F0C09A] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
+              <div className="absolute p-5 md:p-0 md:top-8 md:left-8 z-10">
+                <h4 className="text-2xl md:text-3xl font-semibold mb-1">
                   Brand & Marketing Teams
                 </h4>
-                <p className="w-[60%]">
+                <p className="w-full h:w-[60%]">
                   Launch campaigns at the speed of thought. Test messages, track
                   resonance, and stay 10 steps ahead.
                 </p>
@@ -105,12 +105,12 @@ const WhoItsForSection = () => {
                 className="object-cover"
               />
             </div>
-            <div className="col-span-full md:col-span-2 h-[400px] relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[250px] before:bg-gradient-to-b before:from-[#407df7] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
-              <div className="absolute top-8 left-8 z-10">
-                <h4 className="text-3xl font-semibold mb-1">
+            <div className="col-span-full md:col-span-2 h-[250px] md:h-[400px] relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[160px] before:md:h-[250px] before:bg-gradient-to-b before:from-[#407df7] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
+              <div className="absolute p-5 md:p-0 md:top-8 md:left-8 z-10">
+                <h4 className="text-2xl md:text-3xl font-semibold mb-1">
                   Political Campaigns <br /> & Advocacy Orgs
                 </h4>
-                <p className="w-[80%]">
+                <p className="w-full md:w-[80%]">
                   Deliver precision-crafted messages to every audience with
                   emotional intelligence powered by AI.
                 </p>
@@ -123,12 +123,12 @@ const WhoItsForSection = () => {
               />
             </div>
             <div className="col-span-full grid grid-cols-2 gap-6">
-              <div className="col-span-full md:col-span-1 h-[400px] relative">
-                <div className="absolute top-8 left-8 z-10">
-                  <h4 className="text-3xl font-semibold mb-1">
+              <div className="col-span-full md:col-span-1 h-[250px] md:h-[400px] relative">
+                <div className="absolute p-5 md:p-0 md:top-8 md:left-8 z-10">
+                  <h4 className="text-2xl md:text-3xl font-semibold mb-1">
                     Content Editors & Assistants
                   </h4>
-                  <p className="w-[60%]">
+                  <p className="w-full h:w-[60%]">
                     Save hours of grunt work. Let Dimlang handle the cuts,
                     captions, and tone, you handle the creative.
                   </p>
@@ -140,12 +140,12 @@ const WhoItsForSection = () => {
                   className="object-cover"
                 />
               </div>
-              <div className="col-span-full md:col-span-1 h-[400px] relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[250px] before:bg-gradient-to-b before:from-[#6D93DC] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
-                <div className="absolute top-8 left-8 z-10">
-                  <h4 className="text-3xl font-semibold mb-1">
+              <div className="col-span-full md:col-span-1 h-[250px] md:h-[400px] relative overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[250px] before:bg-gradient-to-b before:from-[#355eab] before:to-transparent before:blur-[2px] before:pointer-events-none before:z-10">
+                <div className="absolute p-5 md:p-0 md:top-8 md:left-8 z-10">
+                  <h4 className="text-2xl md:text-3xl font-semibold mb-1">
                     Micro-Agencies
                   </h4>
-                  <p className="w-[80%]">
+                  <p className="w-full md:w-[80%]">
                     Scale client content production like never before. Fewer
                     tools. Greater output. Massive efficiency.
                   </p>
