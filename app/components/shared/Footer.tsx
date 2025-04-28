0c48df47ea9d561a3e20c77c475ff90707367be8
Simlex <simlexafol@gmail.com>
2025-04-28 15:52:48 +0100

    fix: update footer layout and styling for improved responsiveness and alignment

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -30,8 +30,8 @@ const footerLinks = {
 const Footer = () => {
   return (
     <footer className="bg-black text-white">
-      <div className="container pb-20">
-        <div className="border-b border-white/50 w-1/2 mx-auto pb-5 mb-12">
+      <div className="container pb-10 md:pb-20">
+        <div className="border-b border-white/50 w-[85%] md:w-1/2 mx-auto pb-5 mb-12">
           <Image
             src={images.logo}
             alt=""
@@ -42,7 +42,7 @@ const Footer = () => {
           />
         </div>
 
-        <div className="md:col-span-8 grid grid-cols-2 md:grid-cols-3 gap-8 w-[70%] mx-auto">
+        <div className="md:col-span-8 flex flex-col md:flex-row md:justify-between justify-start gap-8 w-[60%] mx-auto">
           {Object.entries(footerLinks).map(([key, section]) => (
             <div key={key}>
               <h3 className="text-gray-400 text-sm font-medium mb-4">
@@ -69,35 +69,37 @@ const Footer = () => {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="container mt-12 pt-8 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0"
+        className="container sectionPadding mt-12 pt-8 border-t border-gray-800 flex flex-col-reverse md:flex-row justify-between items-center space-y-4 md:space-y-0"
       >
-        <div className="text-gray-400 text-sm">
+        <div className="text-gray-400 text-sm flex-1 py-8 md:py-0 whitespace-nowrap">
           All right reserved. © {new Date().getFullYear()}
         </div>
 
-        <div className="flex items-center space-x-6 ml-20">
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex items-center space-x-6 md:ml-20">
+            <Link
+              href="/privacy-policy"
+              className="text-gray-400 hover:text-white text-sm transition-colors duration-200"
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              href="/terms"
+              className="text-gray-400 hover:text-white text-sm transition-colors duration-200"
+            >
+              Terms of Service
+            </Link>
+          </div>
+
           <Link
-            href="/privacy-policy"
-            className="text-gray-400 hover:text-white text-sm transition-colors duration-200"
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-white transition-colors duration-200 md:ml-auto"
           >
-            Privacy Policy
-          </Link>
-          <Link
-            href="/terms"
-            className="text-gray-400 hover:text-white text-sm transition-colors duration-200"
-          >
-            Terms of Service
+            <Linkedin size={20} />
           </Link>
         </div>
-
-        <Link
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-400 hover:text-white transition-colors duration-200 ml-auto"
-        >
-          <Linkedin size={20} />
-        </Link>
       </motion.div>
     </footer>
   );
