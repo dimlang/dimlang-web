0f299afcd8e4e67fdbfa5b1328087a220b23e0a2
Simlex <simlexafol@gmail.com>
2025-05-02 14:00:04 +0100

    feat: integrate Toaster component for enhanced notification handling in LayoutWrapper

--- app/components/shared/LayoutWrapper.tsx
+++ app/components/shared/LayoutWrapper.tsx
@@ -1,17 +1,19 @@
-"use client"
+"use client";
 import React, { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import NextTopLoader from "nextjs-toploader";
 import { useApplicationContext } from "@/app/context/ApplicationContext";
 import WaitlistModal from "../modal/WaitlistModal";
+import { Toaster } from "sonner";
 
 type Props = {
   children?: ReactNode;
 };
 
 export default function LayoutWrapper({ children }: Props) {
-  const {isWaitlistModalVisible, setIsWaitlistModalVisible} = useApplicationContext();
+  const { isWaitlistModalVisible, setIsWaitlistModalVisible } =
+    useApplicationContext();
 
   return (
     <>
@@ -26,9 +28,10 @@ export default function LayoutWrapper({ children }: Props) {
         speed={200}
         shadow="0 0 10px #f1fa9e,0 0 5px #ceb0fa"
       />
+      <Toaster richColors position="top-right" />
       <WaitlistModal
-          visibility={isWaitlistModalVisible}
-          setVisibility={setIsWaitlistModalVisible}
+        visibility={isWaitlistModalVisible}
+        setVisibility={setIsWaitlistModalVisible}
       />
 
       <Navbar />
