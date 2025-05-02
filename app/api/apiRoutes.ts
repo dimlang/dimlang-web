b8605a439d25c8625918c7a89c724d9d205c61a4
Simlex <simlexafol@gmail.com>
2025-05-02 13:59:02 +0100

    feat: add ApiRoutes class to define API endpoint URLs for the application

---
+++ app/api/apiRoutes.ts
@@ -0,0 +1,29 @@
+/**
+ * The API routes endpoints
+ */
+export class ApiRoutes {
+  /**
+   * The dev base url for the application
+   */
+  //   static BASE_URL_DEV: string = "http://localhost:3050/";
+
+  /**
+   * The test base url for the application
+   */
+  static BASE_URL_TEST: string = "https://apitest.dimlang.com/";
+
+  /**
+   * The live base url for the application
+   */
+  static BASE_URL_LIVE: string = "https://api.dimlang.com/api/v1";
+
+  /**
+   * The base url being used for the application
+   */
+  static BASE_URL: string = ApiRoutes.BASE_URL_LIVE;
+
+  /**
+   * The route to Waitlist Subscription endpoint
+   */
+  static WaitlistSubscription: string = "/waitlist";
+}
